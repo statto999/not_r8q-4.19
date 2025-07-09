43fbfcb832155641ab933ad2f047e7a441316e11
Bruno Martins <bgcngm@gmail.com>
2025-07-10 00:31:44 +0100

    input: keyboard: Do not report SW_HALL_LOGICAL event
    This is not supported in AOSP.
    
    Change-Id: I28c8f2b94b800dab0a4f624dd12b4714fd600054

--- drivers/input/keyboard/hall_ic_logical.c
+++ drivers/input/keyboard/hall_ic_logical.c
@@ -46,7 +46,6 @@ struct hall_drvdata {
 };
 
 static int hall_logical_status = 0;
-static int hall_backflip_status = 0;
 
 static ssize_t hall_logical_detect_show(struct device *dev,
 				struct device_attribute *attr, char *buf)
@@ -110,15 +109,8 @@ static int logical_hallic_notifier_handler(struct notifier_block *nb,
 			input_sync(logical_hall_dev->input);
 		} else if (hall_status == E_LID_NORMAL) {
 			hall_logical_status = LOGICAL_HALL_OPEN;
-			hall_backflip_status = 0;
 			input_info(true, &logical_hall_dev->input->dev, "%s hall_status = %d (NORMAL)\n", __func__, hall_status);
 			input_report_switch(logical_hall_dev->input, SW_LID, hall_logical_status);
-			input_report_switch(logical_hall_dev->input, SW_HALL_LOGICAL, hall_backflip_status);
-			input_sync(logical_hall_dev->input);
-		} else if (hall_status == E_LID_360) {
-			hall_backflip_status = 1;
-			input_info(true, &logical_hall_dev->input->dev, "%s hall_status = %d (BACK)\n", __func__, hall_status);
-			input_report_switch(logical_hall_dev->input, SW_HALL_LOGICAL, hall_backflip_status);
 			input_sync(logical_hall_dev->input);
 		}
 		
@@ -171,7 +163,6 @@ static int hall_logical_probe(struct platform_device *pdev)
 
 	input->evbit[0] |= BIT_MASK(EV_SW);
 	input_set_capability(input, EV_SW, SW_LID);
-	input_set_capability(input, EV_SW, SW_HALL_LOGICAL);
 
 	input->open = hall_logical_open;
 	input->close = hall_logical_close;
@@ -203,8 +194,6 @@ static int hall_logical_probe(struct platform_device *pdev)
 			logical_hallic_notifier_handler, POGO_NOTIFY_DEV_HALLIC);
 
 	input_report_switch(input, SW_LID, hall_logical_status);
-	input_report_switch(input, SW_HALL_LOGICAL, hall_backflip_status);
-	input_info(true, dev, "%s hall_status = %d backflip_status = %d\n", __func__, hall_logical_status, hall_backflip_status);
 
 	pr_info("%s end", __func__);
 	return 0;
