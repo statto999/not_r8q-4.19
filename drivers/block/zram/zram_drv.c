13a39c1c08a340619fd313c04c67596109580428
skye-pa1n <sonicgames772@gmail.com>
2025-10-12 15:27:35 -0300

    drivers: zram: hardcode minimum zram to 4gb
    I noticed that when multitasking in LineageOS 22.2 the 2gb of zram that are enabled by
    default fills fairly quickly, even with mglru enabled.
    Bump the minimum zram to 4gb (LineageOS Official uses 2gb by default), while
    still keeping more agressive zram/mm settings possible by user side.

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -1750,11 +1750,13 @@ static ssize_t disksize_store(struct device *dev,
 		struct device_attribute *attr, const char *buf, size_t len)
 {
 	u64 disksize;
+	u64 disksize_last;
 	struct zcomp *comp;
 	struct zram *zram = dev_to_zram(dev);
 	int err;
 
 	disksize = memparse(buf, NULL);
+        disksize_last = disksize;
 	if (!disksize)
 		return -EINVAL;
 
@@ -1763,6 +1765,12 @@ static ssize_t disksize_store(struct device *dev,
 		pr_info("Cannot change disksize for initialized device\n");
 		err = -EBUSY;
 		goto out_unlock;
+	}
+
+	if (disksize_last < (u64)4096 * SZ_1M) {
+		disksize = (u64)4096 * SZ_1M;
+	} else {
+		disksize = disksize_last;
 	}
 
 	disksize = PAGE_ALIGN(disksize);
