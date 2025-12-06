a20881b187dc2e8cce451aa2cb613f708ef3ded9
skye-pa1n <sonicgames772@gmail.com>
2025-12-06 13:42:23 -0300

    [HACK] sched/tune: dummify & use uclamp alternatives when possible

--- kernel/sched/tune.h
+++ kernel/sched/tune.h
@@ -1,5 +1,4 @@
 
-#ifdef CONFIG_SCHED_TUNE
 
 #include <linux/reciprocal_div.h>
 
@@ -12,17 +11,8 @@ struct target_nrg {
 	struct reciprocal_value rdiv;
 };
 
-int schedtune_cpu_boost_with(int cpu, struct task_struct *p);
-int schedtune_task_boost(struct task_struct *tsk);
-
-int schedtune_prefer_idle(struct task_struct *tsk);
-
-void schedtune_enqueue_task(struct task_struct *p, int cpu);
-void schedtune_dequeue_task(struct task_struct *p, int cpu);
-
-#else /* CONFIG_SCHED_TUNE */
-
 #define schedtune_cpu_boost_with(cpu, p) 0
+
 #ifdef CONFIG_UCLAMP_TASK
 #define schedtune_task_boost(tsk) uclamp_eff_value(p, UCLAMP_MIN) > 0
 #else
@@ -35,14 +25,11 @@ void schedtune_dequeue_task(struct task_struct *p, int cpu);
 #define schedtune_prefer_idle(tsk) 0
 #endif
 
+#define schedtune_enqueue_task(task, cpu) do { } while (0)
+#define schedtune_dequeue_task(task, cpu) do { } while (0)
+
 #ifdef CONFIG_UCLAMP_TASK
 #define schedtune_prefer_high_cap(tsk) uclamp_boosted(tsk)
 #else
 #define schedtune_prefer_high_cap(tsk) 0
 #endif
-
-#define schedtune_enqueue_task(task, cpu) do { } while (0)
-#define schedtune_dequeue_task(task, cpu) do { } while (0)
-
-#define stune_util(cpu, other_util, walt_load) cpu_util_cfs(cpu_rq(cpu))
-#endif /* CONFIG_SCHED_TUNE */
